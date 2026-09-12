a87ba09ec42e1d3a2786b113a8f1af3ab604406c
harryruto <lambert.clement1@outlook.fr>
2026-09-12 20:25:27 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char nomFichier[100] =  {0};
+
+printf("Nom du fichier a supprimer : ");
+scanf("%s", nomFichier);
+FILE* fichier = fopen(nomFichier, "r+");
+
+
+if (fichier != NULL) {
+    fclose(fichier);
+    remove(nomFichier);
+} else {
+    printf("Merci de mettre ce .exe dans le meme dossier que le fichier a supprimer");
+    }
+
+return 0;
+}
+
